39fbf56951aefb57ea8a19e24ee366966f12c082
Dustin Popp <dpopp07@gmail.com>
2022-10-14 18:48:38 -0500

    disable installing dev tools by default (#9)
    macos prompted me to install them when i tried to use git to clone this repo.
    
    this is also relevant:
    https://stackoverflow.com/questions/15371925/how-to-check-if-command-line-tools-is-installed

--- install/applications.sh
+++ install/applications.sh
@@ -1,7 +1,7 @@
 cd ~
 
 # install developer tools
-xcode-select --install
+# xcode-select --install (i think that macos prompts you for this when you run git)
 
 # install homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
