9185f8afa4793d3ff472f8a078ea3f1eb7deda8d
Dustin Popp <dpopp07@gmail.com>
2025-04-25 13:18:26 -0500

    clean up node installation, among other things
    Signed-off-by: Dustin Popp <dpopp07@gmail.com>

--- install/node.sh
+++ install/node.sh
@@ -1,7 +1,10 @@
 cd ~
 
+# need to have $N_PREFIX set
+brew install n
+
 # install node
 n latest
 
 # configure global install location for npm modules
-npm config set prefix '~/npm-global'
+npm config set prefix "$NPM_GLOBAL"
