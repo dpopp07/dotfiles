7979eefe6504a710879b3414f3b6e46108a6fd48
Dustin Popp <dpopp07@gmail.com>
2025-04-29 13:57:40 -0500

    install more developer tools
    Signed-off-by: Dustin Popp <dpopp07@gmail.com>

--- install/applications-work.sh
+++ install/applications-work.sh
@@ -1,8 +1,11 @@
-# work maching specific applications
+# work machine applications
 
 cd ~
 
 brew install gcc
+brew install delve
 brew install hashicorp/tap/terraform
 brew install python
-brew install pyenv
+brew install uv
+brew install kubernetes-cli
+brew install temporal
--- install/applications.sh
+++ install/applications.sh
@@ -12,21 +12,24 @@ eval "$(/opt/homebrew/bin/brew shellenv)"
 
 # install applications available with brew
 brew install --cask rectangle
+brew install --cask flux
 brew install --cask spotify
 brew install --cask firefox
 brew install --cask slack
 brew install --cask iterm2
+brew install --cask nozbe
 brew install --cask sublime-text
 brew install --cask spotmenu
 brew install --cask drawio
 brew install --cask zoom
+brew install --cask docker
 
 brew install duti
 brew install z
 brew install golang
 brew install golangci-lint
-brew install delve
 brew install git-delta
+brew install postgresql
 
 # install zprezto
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
